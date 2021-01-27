172678249c81383d26a487fc20c20ef4edd64a20
WOODGLIM <78022085+WOODGLIM@users.noreply.github.com>
2021-01-27 20:14:58 +0900

    Initial commit

---
+++ discordbot.py
@@ -0,0 +1,20 @@
+from discord.ext import commands
+import os
+import traceback
+from datetime import datetime, timedelta, timezone
+
+bot = commands.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN']
+
+
+
+
+@bot.command()
+async def wk(ctx):
+    JST = timezone(timedelta(hours=+9), 'JST') 
+    now = datetime.now(JST).strftime('%H:%M')
+    ms = ctx.author.name + "　[" + now + "]"    
+    await ctx.send(ms)
+
+
+bot.run(token)
